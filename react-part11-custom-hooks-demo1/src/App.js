c399f4ec9cab8da8726a1b740193bdb988f639eb
Aditya Nair <adityanair1207@gmail.com>
2023-04-14 21:09:04 +0530

    React custom hooks

---
+++ react-part11-custom-hooks-demo1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ForwardCounter from "./components/ForwardCounter";
+import BackwardCounter from "./components/BackwardCounter";
+
+function App() {
+  return (
+    <>
+      <ForwardCounter />
+      <BackwardCounter />
+    </>
+  );
+}
+
+export default App;
---
+++ react-part11-custom-hooks-demo2/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useCallback } from "react";
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks, // fetchTasks is an alias
+  } = useHttp();
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-part10-http-requests-default-rtdb.firebaseio.com/tasks.json"
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-part10-http-requests-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]); // will lead to infinite loop without useCallback
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </>
+  );
+}
+
+export default App;
