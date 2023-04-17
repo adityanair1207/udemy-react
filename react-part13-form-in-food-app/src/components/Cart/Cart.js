7d1b86eea7d9fa12b646c994d90337ee90cf2d7d
Aditya Nair <adityanair1207@gmail.com>
2023-04-17 13:25:58 +0530

    Adding form to food app

---
+++ react-part13-form-in-food-app/src/components/Cart/Cart.js
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import Modal from "../UI/Modal";
+import styles from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem.";
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            amount={item.amount}
+            onAdd={cartItemAddHandler.bind(null, item)} // bind preconfigures a function for future execution. It allows us to preconfigure the argument that the function will receive when it is executed.
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          />
+        );
+      })}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={styles.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
