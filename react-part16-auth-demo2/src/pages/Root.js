8b496f18a01168f7ab6b9b4cfb1934302e43c0dc
Aditya Nair <adityanair1207@gmail.com>
2023-04-27 17:37:48 +0530

    Authentication

---
+++ react-part16-auth-demo2/src/pages/Root.js
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useLoaderData, useSubmit } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../util/auth";
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  // const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
